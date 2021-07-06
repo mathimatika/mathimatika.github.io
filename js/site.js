6c5749cc4fbc20cae9071d833f48e3e5e0b7ad55
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-07-06 10:29:25 -0600

    Update site.js

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@
 const nav = `
 <div id="nav-wrapper">
     <div id="nav-left">
-        <img src="images/mathimatika.png" alt="Logo" loading="lazy" width="53">
+        <a href="/"><img src="images/mathimatika.png" alt="Logo" loading="lazy" width="53"></a>
     </div>
     <div id="nav-right">
         <ul>
