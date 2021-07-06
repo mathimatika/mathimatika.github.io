3a0248cb0491127a7c99f8fffeaedf6546502acf
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-07-06 15:16:53 -0600

    Update site.js

--- js/site.js
+++ js/site.js
@@ -60,4 +60,4 @@ const observer = new IntersectionObserver((entries, observer) => {
     rootMargin: '0px',
     threshold: 1.0
 });
-observer.observe(document.querySelector("#abt-title"));
+observer.observe(document.querySelector(".abt"));
