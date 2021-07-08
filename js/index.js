49020c2365cccdbf2d2c4fa0fd816b698001c4ca
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-07-07 19:19:01 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ const observer = new IntersectionObserver((entry, observer) => {
     if (entry[0].intersectionRatio > 0) {
         for (const elem of entry[0].target.children) {
             elem.style.opacity = '1';
-            elem.style.transform = "translateY(0px)";
         }
         observer.unobserve(entry[0].target);
     }
