ff13da853b6bc0cd3fbf83ff0aba823d0e61ecae
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-09-21 18:16:46 -0600

    Ok this is kinda bad but whatever

--- js/404.js
+++ js/404.js
@@ -1,7 +1,7 @@
 const msg = [
-    "Did you know that 404 can be expressed as the sum of two perfect squares?",
+    "404 can be expressed as the sum of two perfect squares",
     "404 = 2<sup>2</sup> × 101",
-    "404 is a deficient number.",
+    "404 is a deficient number",
     "194<sub>16</sub> = 404"
 ];
 function rand(n) {
@@ -9,4 +9,4 @@ function rand(n) {
     return r > n - 1 ? r - 1 : r;
 }
 
-document.querySelector("#head-wrapper span").innerHTML += msg[rand(msg.length)];
+document.querySelector("#head-wrapper span").innerHTML += "Did you know that " + msg[rand(msg.length)] + "?";
